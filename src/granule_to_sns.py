bb6a330d0df2cc955395d738f0e447171ab94856
mckadesorensen <dmsorensen@alaska.edu>
2024-04-30 16:18:12 -0800

    feat: add logging

--- src/granule_to_sns.py
+++ src/granule_to_sns.py
@@ -1,10 +1,12 @@
+import json
+import logging
 import os
 
 import boto3
 from mandible.log import init_root_logger, log_errors
 from run_cumulus_task import run_cumulus_task
 
-import json
+log = logging.getLogger(__name__)
 
 
 def generate_message(granule) -> dict:
@@ -19,21 +21,26 @@ def generate_message(granule) -> dict:
                     "uri": f"s3://{file['bucket']}/{file['key']}",
                 }
                 for file in granule["files"]
-            ]
-        }
+            ],
+        },
     }
 
 
-def granule_to_sns(event: dict, _):
+def granule_to_sns(event: dict, _) -> dict:
     client = boto3.client("sns")
     granules = event["input"]["granules"]
 
+    sns_topic_arn = os.getenv("SNS_TOPIC_ARN")
+
     for granule in granules:
-        print(granule)
+        message = json.dumps(generate_message(granule))
+        log.info("Sending message:\n %s\n to SNS topic %s", message, sns_topic_arn)
         client.publish(
-            TopicArn=os.getenv("SNS_TOPIC_ARN"),
-            Message=json.dumps(generate_message(granule)),
+            TopicArn=sns_topic_arn,
+            Message=message,
         )
+
+    return event
 
 
 def lambda_handler(event, context):
