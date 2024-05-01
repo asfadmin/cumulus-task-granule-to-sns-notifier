de13c02241b7d1ad37ae27e777661016437ab44e
mckadesorensen <dmsorensen@alaska.edu>
2024-05-01 13:25:04 -0800

    feat: add sns policy

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -1,3 +1,22 @@
 resource "aws_sns_topic" "new_data_notification" {
   name = "${var.prefix}-new-data-notification"
 }
+
+resource "aws_sns_topic_policy" "new_data_notification_policy" {
+  arn = aws_sns_topic.new_data_notification.arn
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          AWS = var.aws_account_arns
+        },
+        Action   = "sns:Subscribe",
+        Resource = aws_sns_topic.new_data_notification.arn
+      }
+    ]
+  })
+}
+
